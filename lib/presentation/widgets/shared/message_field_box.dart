6db20e19603ff206d33a5ce5f38a29d5d4c81c2a
louysdev <louysdev@gmail.com>
2023-11-20 09:44:20 -0400

    Agregnado comportamiento a text field con un controller

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -6,25 +6,40 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final focusNode = FocusNode();
+
+    final textController = TextEditingController();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+        hintText: "Terminal el mensaje con un (?)",
         enabledBorder: outlineInputBorder,
         focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: Icon(Icons.send_rounded),
           onPressed: () {
-            print("Valor capturado de la caja");
+            final textValue = textController.value.text;
+            print("Valor capturado de la caja ${textValue}");
+            textController.clear();
           },
         ));
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print("Valor capturado: ${value}");
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
