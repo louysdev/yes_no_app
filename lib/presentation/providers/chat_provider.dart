5989436ea6bbc2860564ed5ad0ebdd293813d0bd
louysdev <louysdev@gmail.com>
2023-11-21 00:21:12 -0400

    Usar dio para hacer una peticion get

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
   final ScrollController chatScrollController = ScrollController();
 
   List<Message> messageList = [
@@ -9,10 +11,18 @@ class ChatProvider extends ChangeNotifier {
     Message(text: "Que andas haciendo?", fromWho: FromWho.me),
   ];
 
+  Future<void> herResponse() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+  }
+
   Future<void> sendMessage(String text) async {
     if (text.trim().isEmpty) return;
     final newMessage = Message(fromWho: FromWho.me, text: text);
     messageList.add(newMessage);
+
+    if (text.endsWith("?")) {
+      herResponse();
+    }
 
     notifyListeners();
     moveScrollToBottom();
--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
